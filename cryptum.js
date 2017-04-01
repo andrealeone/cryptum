6347e7bd1b0552aaf20c4ffe17980b3ba47b8ca6
Andrea Leone <andrea@etrentino.it>
2017-04-01 18:13:25 +0200

    0.9.63

--- cryptum.js
+++ cryptum.js
@@ -1358,10 +1358,14 @@
     var SHA512 = C_algo.SHA512 = Hasher.extend({
       _doReset: function(){
         this._hash = new X64WordArray.init([
-          new X64Word.init(0x6a09e667, 0xf3bcc908), new X64Word.init(0xbb67ae85, 0x84caa73b),
-          new X64Word.init(0x3c6ef372, 0xfe94f82b), new X64Word.init(0xa54ff53a, 0x5f1d36f1),
-          new X64Word.init(0x510e527f, 0xade682d1), new X64Word.init(0x9b05688c, 0x2b3e6c1f),
-          new X64Word.init(0x1f83d9ab, 0xfb41bd6b), new X64Word.init(0x5be0cd19, 0x137e2179)
+          new X64Word.init(0x6a09e667, 0xf3bcc908),
+          new X64Word.init(0xbb67ae85, 0x84caa73b),
+          new X64Word.init(0x3c6ef372, 0xfe94f82b),
+          new X64Word.init(0xa54ff53a, 0x5f1d36f1),
+          new X64Word.init(0x510e527f, 0xade682d1),
+          new X64Word.init(0x9b05688c, 0x2b3e6c1f),
+          new X64Word.init(0x1f83d9ab, 0xfb41bd6b),
+          new X64Word.init(0x5be0cd19, 0x137e2179)
         ]);
       },
       _doProcessBlock: function(M, offset){
@@ -1525,10 +1529,14 @@
     var SHA384 = C_algo.SHA384 = SHA512.extend({
       _doReset: function(){
         this._hash = new X64WordArray.init([
-          new X64Word.init(0xcbbb9d5d, 0xc1059ed8), new X64Word.init(0x629a292a, 0x367cd507),
-          new X64Word.init(0x9159015a, 0x3070dd17), new X64Word.init(0x152fecd8, 0xf70e5939),
-          new X64Word.init(0x67332667, 0xffc00b31), new X64Word.init(0x8eb44a87, 0x68581511),
-          new X64Word.init(0xdb0c2e0d, 0x64f98fa7), new X64Word.init(0x47b5481d, 0xbefa4fa4)
+          new X64Word.init(0xcbbb9d5d, 0xc1059ed8),
+          new X64Word.init(0x629a292a, 0x367cd507),
+          new X64Word.init(0x9159015a, 0x3070dd17),
+          new X64Word.init(0x152fecd8, 0xf70e5939),
+          new X64Word.init(0x67332667, 0xffc00b31),
+          new X64Word.init(0x8eb44a87, 0x68581511),
+          new X64Word.init(0xdb0c2e0d, 0x64f98fa7),
+          new X64Word.init(0x47b5481d, 0xbefa4fa4)
         ]);
       },
       _doFinalize: function(){
@@ -1595,10 +1603,12 @@
         return function(cipher){
           return {
             encrypt: function(message, key, cfg){
-              return selectCipherStrategy(key).encrypt(cipher, message, key, cfg);
+              return selectCipherStrategy(key)
+                .encrypt(cipher, message, key, cfg);
             },
             decrypt: function(ciphertext, key, cfg){
-              return selectCipherStrategy(key).decrypt(cipher, ciphertext, key, cfg);
+              return selectCipherStrategy(key)
+                .decrypt(cipher, ciphertext, key, cfg);
             }
           };
         };
